15507bf9f02b6c5c7537ef225cab829d16d52667
chardbres <richard.m.bresnahan@gmail.com>
2019-12-05 23:31:04 -0500

    Commented out all references to shots to aid functionality.

--- app/models/shot.js
+++ app/models/shot.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose')
-
-const shotSchema = new mongoose.Schema({
-  distance: {
-    type: Number,
-    required: true
-  },
-  quality: {
-    type: String
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Club',
-    required: true
-  }
-}, {
-  timestamps: true
-})
-
-module.exports = mongoose.model('Shot', shotSchema)
+// const mongoose = require('mongoose')
+//
+// const shotSchema = new mongoose.Schema({
+//   distance: {
+//     type: Number,
+//     required: true
+//   },
+//   quality: {
+//     type: String
+//   },
+//   owner: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'Club',
+//     required: true
+//   }
+// }, {
+//   timestamps: true
+// })
+//
+// module.exports = mongoose.model('Shot', shotSchema)
