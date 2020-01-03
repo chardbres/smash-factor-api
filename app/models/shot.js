190df619b8275d5590f1b4b71bcfe85b727300c4
chardbres <richard.m.bresnahan@gmail.com>
2020-01-03 11:03:52 -0500

    Un-commented shot schema to attempt integration.

--- app/models/shot.js
+++ app/models/shot.js
@@ -1,20 +1,21 @@
-// const mongoose = require('mongoose')
-//
-// const shotSchema = new mongoose.Schema({
-//   distance: {
-//     type: Number,
-//     required: true
-//   },
-//   quality: {
-//     type: String
-//   },
-//   owner: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'Club',
-//     required: true
-//   }
-// }, {
-//   timestamps: true
-// })
-//
-// module.exports = mongoose.model('Shot', shotSchema)
+// 1/2/20: first attempts at adding a new shots database schema to connect to the clubs
+const mongoose = require('mongoose')
+
+const shotSchema = new mongoose.Schema({
+  distance: {
+    type: Number,
+    required: true
+  },
+  quality: {
+    type: String
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Club',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Shot', shotSchema)
